7b3cc87af3de92e68cf705ea156ed13250105631
Colin Osborn <colin@Colins-MacBook-Pro.local>
2018-05-17 13:02:33 -0600

    node built

--- test/node_test.rb
+++ test/node_test.rb
@@ -5,8 +5,17 @@ require_relative '../lib/node'
 
 class NodeTest < Minitest::Test
 
+  def test_it_exists
+    assert Node
+  end
+
  def test_node_can_be_created
    assert_equal Node, Node.new.class
+ end
+
+ def test_we_can_add_a_value_to_node
+   node = Node.new(6)
+   assert_equal 6, node.value
  end
 
 end
